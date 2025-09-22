09edf56cf29723f404d69fa1e903b7dcb1b813f9
Ishan Biswas <biswas.ishan2006@gmail.com>
2025-09-22 16:20:49 +0530

    Add files via upload

---
+++ OOPS/1_problem.py
@@ -0,0 +1,9 @@
+class programmer:
+    a="Microsoft"
+    def __init__(self,name,salary,language):
+        self.name=name
+        self.salary=salary
+        self.language=language
+  
+p=programmer("Ishan biswas",120000,"Python")
+print(p.name,p.salary,p.language,p.a)        
